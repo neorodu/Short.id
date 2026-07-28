0af4a0837fd689d564036911e9e52da7a65dd221
Neorodu <knur13533@gmail.com>
2026-07-28 23:47:24 +0700

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'neoneq-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './player.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate - clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch - cache first, then network
+self.addEventListener('fetch', (event) => {
+  // Skip non-GET and external audio (blob/http audio files)
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        // Don't cache non-success or non-basic
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
